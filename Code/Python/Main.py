c1be23a5424b5f2174293eb625d22853da258d29
Little-UWU-Femboy <gabrielcenteno2003@icloud.com>
2026-02-27 15:58:32 -0600

    Learning Sync

--- Code/Python/Main.py
+++ Code/Python/Main.py
@@ -1,4 +1,16 @@
+import pandas
+
 with open("Input.txt", "rb") as fp:
     pass
 
+print("This is a test")
+
+print("This might be a scary thing")
+
+df = pandas.DataFrame([{"Name": 1, "Age": 2}])
+
+print(df)
+
 print(fp.mode)
+
+print("This is a good thing")
