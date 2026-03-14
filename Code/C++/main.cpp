d825a51ac95b8f8bc9f1e2a1ccbaf38c30c713ae
Little-UWU-Femboy <dehydratedfetus@gmail.com>
2026-03-14 15:10:12 -0500

    Learning Sync

---
+++ Code/C++/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::string name = "Jack";
+    std::string name2 = "Bless";
+    std::string name3 = name + " " + name2;
+    std::cout << name3 << std::endl;
+    
+    std::cout << 4 << std::endl;
+
+    return 0;
+}
