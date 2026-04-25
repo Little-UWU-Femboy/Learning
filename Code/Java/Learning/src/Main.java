42810920a27c927bb10d9c099ddd2cceb5d380c5
Little-UWU-Femboy <dehydratedfetus@gmail.com>
2026-04-25 17:35:53 -0500

    Learning Sync

--- Code/Java/Learning/src/Main.java
+++ Code/Java/Learning/src/Main.java
@@ -2,9 +2,9 @@ class Main{
     public static void main(String[] args){
     	Employee tmp = new Employee();
     	tmp.wrapper();
-    	
+
     	Point p = new Point(5);
-    	
+
     	System.out.println(p.p());
     }
 }
